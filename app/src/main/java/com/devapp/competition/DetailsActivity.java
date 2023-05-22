1377bf240afa245b39e82eccc779798894e9885b
sazgez <wishell.rock.4@gmail.com>
2023-05-22 11:11:01 +0300

    New activity is created
    DetailsActivity is created and Intent is used for navigating between activities.

---
+++ app/src/main/java/com/devapp/competition/DetailsActivity.java
@@ -0,0 +1,29 @@
+package com.devapp.competition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    Button buttonReady;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        // binding views
+        buttonReady = (Button) findViewById(R.id.buttonReady);
+    }
+
+    public void buttonClick(View view) {
+        //intent = new Intent(DetailsActivity.this, ???); // creating the intent
+        finish(); // finalizing the DetailsActivity
+        startActivity(intent); // navigating to the ???
+    }
+}
--- app/src/main/java/com/devapp/competition/MainActivity.java
+++ app/src/main/java/com/devapp/competition/MainActivity.java
@@ -5,24 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button buttonDetails, buttonStart;
     Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // binding views
+        buttonDetails = (Button) findViewById(R.id.buttonDetails);
+        buttonStart = (Button) findViewById(R.id.buttonStart);
     }
 
     public void buttonClick(View view) {
         switch (view.getId()) {
             case R.id.buttonDetails:
-                // intent config
+                intent = new Intent(MainActivity.this, DetailsActivity.class); // creating the intent
+                finish(); // finalizing the MainActivity
+                startActivity(intent); // navigating to the DetailsActivity
                 break;
             case R.id.buttonStart:
-                // intent config
+                //intent = new Intent(MainActivity.this, ???); // creating the intent
+                finish(); // finalizing the MainActivity
+                startActivity(intent); // navigating to the ???
                 break;
         }
     }
