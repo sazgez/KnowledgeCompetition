947552d6eb1ce4ab9be6f7d08460cb79c9f7bef5
sazgez <wishell.rock.4@gmail.com>
2023-05-23 15:27:31 +0300

    New layout for the Score button is created
    The Score button gained functionality through AlertDialog and LayoutInflater. Image Asset was used by adding an icon for the layout of the Score button.

--- app/src/main/java/com/devapp/competition/CategoryActivity.java
+++ app/src/main/java/com/devapp/competition/CategoryActivity.java
@@ -1,13 +1,20 @@
 package com.devapp.competition;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.zip.Inflater;
 
 public class CategoryActivity extends AppCompatActivity {
 
@@ -20,6 +27,8 @@ public class CategoryActivity extends AppCompatActivity {
                 R.drawable.book, R.drawable.crypto};
     Adapter adapter;
     Intent intent;
+    AlertDialog.Builder dialog;
+    LayoutInflater inflater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +39,8 @@ public class CategoryActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.listView);
         buttonScore = (Button) findViewById(R.id.buttonScore);
         adapter = new Adapter(category, image, CategoryActivity.this); // creating the adapter
+        dialog = new AlertDialog.Builder(CategoryActivity.this); // creating the AlertDialog
+        inflater = LayoutInflater.from(CategoryActivity.this); // set the inflater
         listView.setAdapter(adapter); // set the adapter to list
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -43,6 +54,9 @@ public class CategoryActivity extends AppCompatActivity {
     }
 
     public void buttonClick(View view) {
-        // Score button click
+        view = inflater.inflate(R.layout.score, null); // bound the layout to be showed
+        dialog.setView(view); // set the view into the AlertDialog
+        dialog.setCancelable(true); // clicking anywhere on screen will dismiss the dialog
+        dialog.show(); // showing the layout
     }
 }
